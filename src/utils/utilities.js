d7cb761316ec6b5a402e30e127ded9cff27cf44c
Nik <NikBaltatzis@gmail.com>
2018-10-13 10:28:45 -0400

    getInitials function added, win percentage has only 1 minimum game required

--- src/utils/utilities.js
+++ src/utils/utilities.js
@@ -5,12 +5,24 @@ let percent = function(portion, total){
 let percentOfPlayerWins = function(wins, losses){
   let total = wins + losses;
   let winPercent = percent(wins, total); // returns number, NaN or Infinity/-Infinity
-  return total > 9 && !!winPercent && isFinite(winPercent) ? winPercent + '%' : '-'; // Ensures we have a sensible result
+  return total > 0 && !!winPercent && isFinite(winPercent) ? winPercent + '%' : '-'; // Ensures we have a sensible result
 }
 
 const daysSince = (timestamp) => Math.floor((Date.now() - timestamp) / 1000 / 60 / 60 / 24);
 
+const getInitials = name => {
+  if (name && name.length > 0) {
+    let names = name.split(' ');
+    if (names.length > 4) {
+      names = names.slice(0, 4);  
+    }
+
+    return names.reduce((initials, name) => `${initials}${name.charAt(0)}`, '');
+  }
+}
+
 module.exports = {
   percentOfPlayerWins,
-  daysSince
+  daysSince,
+  getInitials,
 };
